665bf7f426ff4e11f5633e1cb97d04d1de0ed803
darshan761 <darshanpatil761@gmail.com>
2018-11-01 00:17:48 +0530

    seventh  commit

---
+++ app/src/main/java/com/example/hp/moodle/grading.java
@@ -0,0 +1,124 @@
+package com.example.hp.moodle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class grading extends AppCompatActivity {
+    private SeekBar seekBar,seekBar1,seekBar2,seekBar3;
+    private Button submit;
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+    int p1,p2,p3,p4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grading);
+        seekBar = findViewById(R.id.p1);
+        seekBar1 = findViewById(R.id.p2);
+        seekBar2 = findViewById(R.id.p3);
+        seekBar3 = findViewById(R.id.p4);
+        submit = findViewById(R.id.submit);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                p1 = progress/10;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                p2 = progress/10;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                p2 = progress/10;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                p3 = progress/5;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int total = p1 + p2 + p3 + p4;
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+                ref.child("Exp").child(getIntent().getStringExtra("MEME")).orderByChild("student_name").equalTo(auth.getCurrentUser().getEmail()).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for(DataSnapshot x : dataSnapshot.getChildren()){
+                            x.child("p1").getRef().setValue(p1);
+                            x.child("p1").getRef().setValue(p1);
+                            x.child("p1").getRef().setValue(p1);
+                            x.child("p1").getRef().setValue(p1);
+                            x.child("Total").getRef().setValue(total);
+
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+                Toast.makeText(v.getContext(),"Graded ",Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/hp/moodle/myAdapter3.java
@@ -0,0 +1,71 @@
+package com.example.hp.moodle;
+
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by HP on 30-10-2018.
+ */
+public class myAdapter3 extends RecyclerView.Adapter<myAdapter3.MyViewHolder> {
+    private ArrayList<String> mDataset ;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+        public MyViewHolder(TextView v) {
+            super(v);
+            mTextView = v;
+        }
+    }
+
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public myAdapter3(ArrayList<String> myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public myAdapter3.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                      int viewType) {
+        // create a new view
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.my_text_view, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTextView.setText(mDataset.get(position));
+        holder.mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             Intent i = new Intent(v.getContext(),grading.class);
+             v.getContext().startActivity(i);
+            }
+        });
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        Log.d("LENGTH",Integer.toString(mDataset.size()));
+        return mDataset.size();
+    }
+}
